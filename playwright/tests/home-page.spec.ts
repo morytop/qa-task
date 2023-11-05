4bff979f8699e682bed0068a6fbe2fdae55176ea
Piotr Moryto <p.moryto@tidio.net>
2023-11-05 19:18:43 +0100

    Add test with 2 premium and 7 economy rooms

--- playwright/tests/home-page.spec.ts
+++ playwright/tests/home-page.spec.ts
@@ -2,9 +2,15 @@ import { HomePage } from '../page/home.page';
 import { expect, test } from '@playwright/test';
 
 test.describe('Verify occupancy and hotel profit', () => {
-  test('Check with Premium rooms: 7 and Economy rooms: 5', async ({ page }) => {
-    const homePage = new HomePage(page);
+  let homePage: HomePage;
+
+  test.beforeEach(async ({ page }) => {
+    homePage = new HomePage(page);
     await homePage.goto();
+  });
+
+  test('Check with Premium rooms: 7 and Economy rooms: 5', async ({ page }) => {
+    homePage = new HomePage(page);
     await homePage.calculateRoomsOccupancy('7', '5');
     const freePremiumRooms = 1;
     const freeEconomyRooms = 1;
@@ -18,6 +24,48 @@ test.describe('Verify occupancy and hotel profit', () => {
     ).toBeInViewport();
     await expect(
       page.getByText(`Usage Premium: 6 (EUR ${usagePremium})`),
+    ).toBeInViewport();
+    await expect(
+      page.getByText(`Usage Economy: 4 (EUR ${usageEconomy})`),
+    ).toBeInViewport();
+  });
+
+  test('Check with Premium rooms: 2 and Economy rooms: 7', async ({ page }) => {
+    homePage = new HomePage(page);
+    await homePage.calculateRoomsOccupancy('2', '7');
+    const freePremiumRooms = 0;
+    const freeEconomyRooms = 3;
+    const usagePremium = 583;
+    const usageEconomy = 189;
+    await expect(
+      page.getByText(`Free Premium rooms: ${freePremiumRooms}`),
+    ).toBeInViewport();
+    await expect(
+      page.getByText(`Free Economy rooms: ${freeEconomyRooms}`),
+    ).toBeInViewport();
+    await expect(
+      page.getByText(`Usage Premium: 2 (EUR ${usagePremium})`),
+    ).toBeInViewport();
+    await expect(
+      page.getByText(`Usage Economy: 4 (EUR ${usageEconomy})`),
+    ).toBeInViewport();
+  });
+
+  test('Check with Premium rooms: 7 and Economy rooms: 1', async ({ page }) => {
+    homePage = new HomePage(page);
+    await homePage.calculateRoomsOccupancy('7', '1');
+    const freePremiumRooms = 0;
+    const freeEconomyRooms = 3;
+    const usagePremium = 583;
+    const usageEconomy = 189;
+    await expect(
+      page.getByText(`Free Premium rooms: ${freePremiumRooms}`),
+    ).toBeInViewport();
+    await expect(
+      page.getByText(`Free Economy rooms: ${freeEconomyRooms}`),
+    ).toBeInViewport();
+    await expect(
+      page.getByText(`Usage Premium: 2 (EUR ${usagePremium})`),
     ).toBeInViewport();
     await expect(
       page.getByText(`Usage Economy: 4 (EUR ${usageEconomy})`),
